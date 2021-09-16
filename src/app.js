f825149a9f3d322c9b0f973197b63ce6e77640d3
wayy <948840255@qq.com>
2021-09-16 23:18:12 +0800

    将 svg 代码整理到 icon.vue

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@
 //全局button组件
 import Vue from "vue";
 import Button from './button'
+import Icon from './icon'
 //声明
 Vue.component('g-button',Button)
-
+Vue.component('g-icon', Icon)
 new Vue({
     el: '#app',
 })
