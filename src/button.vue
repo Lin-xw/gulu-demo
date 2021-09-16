ab9706a58c5bc8a372d8e45e575d1df71d50238d
wayy <948840255@qq.com>
2021-09-16 22:30:19 +0800

    添加icon 和iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  props: ['icon','iconPosition']
+}
 </script>
 <style lang="scss">
 .g-button {
@@ -12,14 +21,21 @@ export default {}
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover {/*用&表示当前的选择器*/
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align:middle;
+  &:hover { /*用&表示当前的选择器*/
     border-color: var(--border-color-hover);
   }
-  &:active {
-    background-color: var(--button-active-bg);
+  &:active {background-color: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .content{order:2;}
+  > .icon{order:1; margin-right:.1em;}
+
+  &.icon-right{
+    > .content{order:1;}
+    > .icon{ order:2; margin-right: 0;margin-left:.1em;}
   }
-  &:focus {
-    outline: none;
-}
 }
 </style>
