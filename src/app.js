ae8a0d29542d1835fbf470dff812bdf7e1c326bf
wayy <948840255@qq.com>
2021-09-25 20:47:29 +0800

    row 和 col 的雏形完成

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 //声明
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
-
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 new Vue({
     el: '#app',
     data: {
