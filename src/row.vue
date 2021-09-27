0ea35c41daef38405bd7233e3d7066bd41f6535f
wayy <948840255@qq.com>
2021-09-27 11:08:30 +0800

    模仿淘宝页面做示例

--- src/row.vue
+++ src/row.vue
@@ -1,32 +1,56 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
 <script>
-  export default {
-    name:'GuluRow',
-    props:{
-      gutter:{
-        type:[Number,String]
-      }
+export default {
+  name: 'GuluRow',
+  props: {
+    gutter: {
+      type: [Number, String]
     },
-    computed:{
-      rowStyle(){
-        let {gutter} = this
-        return {marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}
+    align: {//排列对齐验证
+      type: String,
+      validator(value) {
+        return ['left', 'right', 'center'].includes(value);
       }
-    },
-    mounted() {
-      this.$children.forEach((vm) =>{
-        vm.gutter = this.gutter
-      })
     }
+
+  },
+  computed: {
+    rowStyle() {
+      let {gutter} = this
+      return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+    },
+    rowClass() {
+      let {align} = this
+      return [align && `align-${align}`]
+    }
+  },
+
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
   }
+}
 </script>
 <style lang="scss" scoped>
-.row{
+.row {
   display: flex;
+
+  &.align-left {
+    justify-content: flex-start;
+  }
+
+  &.align-right {
+    justify-content: flex-end;
+  }
+
+  &.align-center {
+    justify-content: center;
+  }
 }
 
 </style>
