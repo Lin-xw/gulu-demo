5b7516373d8d5ad35fbe08d1c40c7be69b21c10b
wayy <948840255@qq.com>
2021-09-29 10:15:32 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 <script>
+let validator = (value)=>{
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {//遍历
+    //一旦发现有一个key不在这两个里面就等于false
+    if (!['span', 'offset'].includes(key)) {
+      valid = false
+    }
+  })
+  return valid
+}
 export default {
   name: 'GuluCol',
   props: {
@@ -12,20 +23,33 @@ export default {
     },
     offset: {
       type: [Number, String]
-    }
+    },
+    phone: {type: Object, validator,},
+    ipad: {type: Object, validator,},
+    narrowPc: {type: Object, validator,},
+    pc: {type: Object, validator,},
+    widePc: {type: Object, validator,}
   },
-  data() {
-    return {
-      gutter: 0
-    }
-  },
+    data() {
+      return {
+        gutter: 0
+      }
+    },
   computed: {
     colClass() {
-      let {span, offset} = this
+      let {span, offset, phone,ipad,narrowPc,pc,widePc} = this
+      let phoneClass = [] //默认为空
       return [
         span && `col-${span}`,
-        offset && `offset-${offset}`
+        offset && `offset-${offset}`,
+        //如果有***就传col-***-$-{***.span}
+        ...(phone && [`col-phone-$-{phone.span}`]),
+        ...(ipad && [`col-ipad-$-{ipad.span}`]),
+        ...(narrowPc && [`col-narrowPc-$-{narrowPc.span}`]),
+        ...(pc && [`col-pc-$-{pc.span}`]),
+        ...(widePc && [`col-widePc-$-{widePc.span}`]),
       ]
+      //把传给js的东西体现在标签上面，方可用css切换
     },
     colStyle() {
       return {
@@ -47,7 +71,82 @@ export default {
   $class-prefix: offset-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
-      margin-left: ($n / 24) * 100%;
+
+    }
+  }
+  //手机屏幕
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+    $class-prefix: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  //ipad屏幕
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  //窄屏幕
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  //电脑屏幕
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  //宽屏电脑
+  @media (min-width: 1201px){
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
   }
 }
