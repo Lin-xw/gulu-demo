402f39019fbb6a08f75834aed8b79e57eede3cfa
wayy <948840255@qq.com>
2021-09-30 11:19:00 +0800

    添加简单滑动

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="sider">
+  <transition name="fade">
+  <div class="sider" v-if="visible">
     <slot></slot>
+    <button @click="visible=false">111111111111</button>
   </div>
+  </transition>
 </template>
 <script>
   export default {
-    name:'GuluSider'
+    name:'GuluSider',
+    data(){
+      //实现点击button后页面变没
+      return {
+        visible: true
+      }
+    },
+    methods:{//钩子
+      enter: function (el,done){
+        //...
+        done()
+      },
+      leave: function (el,done){
+        //...
+        done()
+      },
+    }
   }
 </script>
 <style lang="scss" scoped>
 .sider{
-
+  position: relative;
+  >button{
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.fade-enter-active,.fade-leave-active{
+  transition: all .5s;
+}
+.fade-enter,.fade-leave-to{
+  margin-left: -200px;
 }
 </style>
