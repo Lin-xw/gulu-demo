06598aa1c511e0bdec8fc9e8ab1332b43384f075
wayy <948840255@qq.com>
2021-10-05 22:04:50 +0800

    四个方向的位置

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible"
+    :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
@@ -15,25 +16,52 @@ export default {
     //默认看不见
     return {visible: false}
   },
+  props: {
+    position: {//添加气泡四个方向选择
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    }
+  },
   methods: {
     positionContent() {
+      const {contentWrapper, triggerWrapper} = this.$refs
       //位置内容
       //让popover显示在外面
       document.body.appendChild(this.$refs.contentWrapper)
-      //获取元素的位置，使popover出现在按钮的上方
-      let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      //获取元素的位置，使popover出现在按钮的4个方向
+      const {width, height, top, left} = triggerWrapper.getBoundingClientRect()
+      const {height: height2} = contentWrapper.getBoundingClientRect()
+      let positions = {
+        top: {top: top + window.scrollY, left: left + window.scrollX,},
+        bottom: {top: top + height + window.scrollY, left: left + window.scrollX},
+        left: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX
+        },
+        right: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX + width
+        },
+      }
+      contentWrapper.style.left = positions[this.position].left + 'px'
+      contentWrapper.style.top = positions[this.position].top + 'px'
     },
     onClickDocument(e) {
       //监听点击事件
       //如果包含了e.target，就什么都不做，否则执行关闭
       if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
-      ) {return}
+      ) {
+        return
+      }
       if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.contentWrapper.contains(e.target))
-      ) {return}
+      ) {
+        return
+      }
       this.close()
     },
     open() {
@@ -64,43 +92,90 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-$border-radius: 4px;
+<style scoped lang="scss">
 $border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
   position: relative;
 }
-
 .content-wrapper {
   position: absolute;
-  padding: .5em 1em;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
   filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
   background: white;
-  transform: translateY(-100%);
-  border-color: $border-color;
-  border-radius: $border-radius;
-  margin-top: -10px;
+  padding: .5em 1em;
   max-width: 20em;
   word-break: break-all;
-
-  &::before, &::after{
+  &::before, &::after {
     content: '';
     display: block;
     border: 10px solid transparent;
     width: 0;
     height: 0;
     position: absolute;
-    top: 100%;
   }
-
-  &::before{;
-    top: 100%;
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before, &::after {
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
-  &::after {
-    border-top-color: white;
-    top: calc(100% - 1px);
+  &.position-bottom {
+    margin-top: 10px;
+    &::before, &::after {
+      left: 10px;
+    }
+    &::before {
+      border-bottom-color: black;
+      bottom: 100%;
+    }
+    &::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%);
+    margin-left: -10px;
+    &::before, &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-left-color: black;
+      left: 100%;
+    }
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before, &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-right-color: black;
+      right: 100%;
+    }
+    &::after {
+      border-right-color: white;
+      right: calc(100% - 1px);
+    }
   }
 }
 </style>
