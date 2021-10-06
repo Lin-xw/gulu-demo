c0739c585496f72ded7c95bee20963dfe3288126
wayy <948840255@qq.com>
2021-10-06 15:58:24 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
-      {{title}}
+    <div class="title" @click="toggle">
+      {{ title }}
     </div>
     <div class="content" v-if="open">
       <slot></slot>
@@ -10,52 +10,80 @@
 </template>
 
 <script>
- export default {
-  name:'GuluCollapseItem',
-   props:{
-    title:{
-      type:String,
-      required:true
+export default {
+  name: 'GuluCollapseItem',
+  props: {
+    title: {
+      type: String,
+      required: true
     }
-   },
-   data(){
+  },
+  data() {
     return {
-      open:false
+      open: false
     }
-   }
+  },
+  inject: ['eventBus'],//从父组件传入
+  mounted() {
+    //监听
+    //看被选中的vm是不是自己，是自己就发出来
+    //如果有 this.eventBus && 在监听
+    this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
+      if(vm !== this){
+        this.close()
+      }
+    })
+  },
+  methods:{
+    toggle() {
+      if (this.open) {
+        this.open = false
+      } else {
+        this.open = true
+        //触发事件，被选中哪个就哪个this传进去
+        this.eventBus && this.eventBus.$emit('update:selected', this)
+      }
+    },
 
+    close(){
+      this.open = false
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-  $grey: #ddd;
-  $border-radius: 4px;
-  .collapseItem{
-    >.title{
-      border: 1px solid $grey;
-      border-radius: $border-radius;
-      margin-top: -1px;
-      margin-left: -1px;
-      margin-right: -1px;
-      min-width: 32px;
-      display: flex;
-      align-items: center;
-      padding: 0 8px;
-    }
-    &:first-child{
-      >.title{
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
-    }
-    &:last-child{
-      >.title:last-child{
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
-    }
-    >.content{
-      padding: 8px;
+$grey: #ddd;
+$border-radius: 4px;
+.collapseItem {
+  > .title {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+    margin-top: -1px;
+    margin-left: -1px;
+    margin-right: -1px;
+    min-width: 32px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
+  }
+
+  &:first-child {
+    > .title {
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
     }
   }
+
+  &:last-child {
+    > .title:last-child {
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
+    }
+  }
+
+  > .content {
+    padding: 8px;
+  }
+}
 </style>
