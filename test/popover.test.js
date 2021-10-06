e27d8712ded78afc6eebc14eeee05c9c4a615713
wayy <948840255@qq.com>
2021-10-06 10:50:33 +0800

    popover 有一个测试用例不知道如何实现，先跳过

--- test/popover.test.js
+++ test/popover.test.js
@@ -6,7 +6,7 @@ import Popover from '../src/popover'
 import Col from "../src/col";
 import {Button} from "../index";
 
-Vue.component('g-popover',Popover)
+Vue.component('g-popover', Popover)
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
@@ -17,13 +17,12 @@ describe('Popover', () => {
     expect(Popover).to.exist
   })
   it('可以设置position.', () => {
-
     //放入页面
     const div = document.createElement('div')
     document.body.appendChild(div)
     //造css结构
     div.innerHTML = `
-      <g-popover position="bottom">
+      <g-popover position="bottom" ref="a">
         <template slot="content">
             弹出内容
         </template>
@@ -36,13 +35,33 @@ describe('Popover', () => {
     //构造vm实例
     // console.log(vm.$el.outerHTML);
     // console.log(vm.$refs.popoverWrapper);
-    vm.$nextTick(()=>{
-vm.$el.querySelector('button').click()
-      vm.$nextTick(()=>{
-        const {contentWrapper} = vm.$refs.a.$refs
-        expect(contentWrapper.classList.contains('position-bottom')).to.be.true
-      })
-
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+    })
+  })
+  it('可以设置 trigger',()=>{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    //造css结构
+    div.innerHTML = `
+      <g-popover trigger="hover" ref="a">
+        <template slot="content">
+            弹出内容
+        </template>
+        <button >点我</button>
+    </g-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    let event = new Event('mouseenter');
+    vm.$el.dispatchEvent(event)
+    //期待存在
+    vm.$nextTick(() => {
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper.classList.contains('position-bottom')).to.be.true
     })
   })
 })
